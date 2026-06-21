a50cc48e89ca67a032a130f9f217fefae07b4a90
Lakshmi Bhavani Katta <kattalakshmibhavani2110@gmail.com>
2026-06-21 22:49:29 +0530

    Update reflectance_multiplier.v

--- rtl/reflectance_multiplier.v
+++ rtl/reflectance_multiplier.v
@@ -1,33 +1,12 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 20.06.2026 14:14:22
-// Design Name: 
-// Module Name: reflectance_multiplier
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-`timescale 1ns / 1ps
 
 module reflectance_multiplier(
 
     input  wire [7:0]  pc,
     input  wire [31:0] reciprocal,
 
-    output wire [7:0] reflectance
+   output wire [15:0] reflectance
+
 
 );
 
@@ -35,7 +14,7 @@ wire [39:0] product;
 
 assign product = pc * reciprocal;
 
-assign reflectance = product[23:16];
-// Equivalent to product >> 16
+// Remove Q0.32 scaling
+assign reflectance = product >> 8;
 
 endmodule
